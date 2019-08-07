2dfe4577ab3e2708cfbdd2f33517aba94cdaa5f5
0x332 <kinspotting@protonmail.com>
2019-08-07 16:13:49 +0500

    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.send('Get ready for OpenSea!');
+  res.send('test test');
 })
 
 app.get('/api/token/:token_id', function(req, res) {
